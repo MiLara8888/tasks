0bc0b973cb7c374065d8e41ba582ffb0beefe2eb
MiLara8888 <75615953+MiLara8888@users.noreply.github.com>
2021-08-18 16:19:07 +0300

    Add files via upload

---
+++ selenium_and_os.py
@@ -0,0 +1,31 @@
+#Напишите скрипт, который будет выполнять следующий сценарий:
+
+#Открыть страницу http://suninjuly.github.io/file_input.html
+#Заполнить текстовые поля: имя, фамилия, email
+#Загрузить файл. Файл должен иметь расширение .txt и может быть пустым
+#Нажать кнопку "Submit"
+
+
+from selenium.webdriver.support.ui import Select
+from selenium import webdriver
+import time
+from math import *
+import os
+current_dir = os.path.abspath(os.path.dirname(__file__))    # получаем путь к директории текущего исполняемого скрипта
+file_name = "numbers.txt"     # имя файла, который будем загружать на сайт
+file_path = os.path.join(current_dir, file_name)      # получаем путь к загружаемому файлу
+try:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/file_input.html")
+    name=browser.find_element_by_css_selector("[placeholder='Enter first name']").send_keys('Lara')
+    last=browser.find_element_by_css_selector("[placeholder='Enter last name']").send_keys('Mir')
+    mail = browser.find_element_by_css_selector("[placeholder='Enter email']").send_keys('Mir@mail')
+    file=browser.find_element_by_css_selector('[id="file"]').send_keys(file_path)           # отправляем файл
+    btn=browser.find_element_by_css_selector('.btn').click()
+
+finally:
+
+    time.sleep(30)
+
+    browser.quit()
+
